19245946ad50c5513fbcffd8f589fe5493308862
rishikeshpatange <82082051+rishikeshpatange@users.noreply.github.com>
2022-07-26 21:19:05 +0530

    added new project

--- src/components/Home_work.js
+++ src/components/Home_work.js
@@ -14,26 +14,21 @@ function Home_work() {
                 <div>
                     <a href="https://diskt2.web.app/" target="_blank">
 
-                    <span className='home_work_img_box'>
-                        <img  src="./Images/dares.jpg" alt="" />
-                    </span>
-                    <p>Distk</p>
+                        <img src="./Images/project4.png" alt="" />
+                        <p>Distk</p>
                     </a>
                 </div>
 
                 <div data-aos="fade-up">
                     <a href="https://ecommerce-c7689.web.app/" target="_blank">
-
-                    <span className='home_work_img_box home_work_img_box2'>
-                        <img src="./Images/ecommerce.jpg" alt="" />
-                    </span>
-                    <p>Ecommerce</p>
+                        <img src="./Images/project1.png" alt="" />
+                        <p>Ecommerce</p>
                     </a>
                 </div>
             </div>
             <Link to="/Work">
-            <button className='home_work_btn'>More work</button>
-                </Link>
+                <button className='home_work_btn'>More work</button>
+            </Link>
         </div>
     )
 }
