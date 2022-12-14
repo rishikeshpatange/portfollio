fc2205958ba8613400e92435d02244f35ec85ab1
rishikeshpatange <82082051+rishikeshpatange@users.noreply.github.com>
2022-12-14 22:30:17 +0530

    new banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -49,6 +49,7 @@ function Banner() {
 
     const FadeUp = {
         initial: {
+            
             translateY: 200,
             opacity: 0
         },
@@ -74,7 +75,7 @@ function Banner() {
 
             transition: {
                 duration: 0.5,
-                delay: 0.6
+                delay: 0.1
             }
         }
 
@@ -83,22 +84,30 @@ function Banner() {
         <div className='banner'>
 
             <div className="banner_container">
-                <motion.img variants={BannerFadeUp} initial="initial" animate="animate" transition={{delay:1}}  src="/Images/banner.png" className='banner_pic' alt="" />
+
                 <div className='banner_content'>
-                    <motion.span variants={banner_left} initial="initial" animate="animate">
-                        <p>Based in Pune</p>
-                        <motion.img variants={globe_animation}
-                            initial="initial"
-                            animate="animate" className='globe_image' src="/Images/globe.png" alt="" />
-                    </motion.span>
-                    <motion.h1 variants={banner_right} initial="initial" animate="animate">Freelance <br /> Designer & React Developer</motion.h1>
+                    <div className='banner_content_left'>
+
+                        <motion.h1 variants={banner_left} initial="initial" animate="animate">Freelance</motion.h1>
+                        <motion.h1 variants={banner_left} initial="initial" animate="animate"> Designer & React Developer</motion.h1>
+                        <motion.span variants={banner_left} initial="initial" animate="animate">
+                            <p>Based in Pune</p>
+                            <motion.img variants={globe_animation}
+                                initial="initial"
+                                animate="animate" className='globe_image' src="/Images/globeDark.png" alt="" />
+                        </motion.span>
+                    </div>
+
+                    <motion.img variants={banner_right} initial="initial" animate="animate" transition={{ delay: 1 }} src="/Images/banner.png" className='banner_pic' alt="" />
+
                 </div>
+
                 <motion.div variants={FadeUp} initial="initial" animate="animate" className='marquee'>
                     <Marquee
                         gradient={0}
-                        speed={90}
+                        speed={130}
                         loop={0}
-                        direction={'right'}
+                        direction={'left'}
                     >
                         <h1 className='marquee1'> Rishikesh Patange  ― </h1>
                     </Marquee>
@@ -108,11 +117,28 @@ function Banner() {
             {/* banner phone */}
 
             <div className="banner_container_phone">
-            <motion.img variants={BannerFadeUp} initial="initial" animate="animate" transition={{delay:1}}  src="/Images/banner.png" className='banner_pic' alt="" />
+                <motion.img variants={BannerFadeUp} initial="initial" animate="animate" transition={{ delay: 1 }} src="/Images/banner.png" className='banner_pic' alt="" />
 
                 <motion.div variants={FadeUp} initial="initial" animate="animate" >
 
-                    <Marquee
+               
+                </motion.div>
+
+                <div className='banner_content_phone'>
+                    <motion.h1 variants={banner_left} initial="initial" animate="animate">Freelance</motion.h1>
+                    <motion.h1 variants={banner_left} initial="initial" animate="animate">Designer & Developer</motion.h1>
+
+                    <div className='phone_basedIn'>
+                        <p>Based in Pune, India</p>
+                    <motion.img variants={globe_animation}
+                        initial="initial"
+                        animate="animate" className='globe_image_phone' src="/Images/globeDark.png" alt="" />
+                        
+                    </div>
+                    
+                        
+                </div>
+                <Marquee
                         className='marquee_phone'
                         gradient={0}
                         speed={70}
@@ -121,14 +147,6 @@ function Banner() {
                         <h1> Rishikesh ― Patange  ― </h1>
 
                     </Marquee>
-                </motion.div>
-
-                <div className='banner_content_phone'>
-                    <motion.h1 variants={banner_left} initial="initial" animate="animate">Freelance <br />  Designer & Developer</motion.h1>
-                    <motion.img variants={globe_animation}
-                        initial="initial"
-                        animate="animate" className='globe_image_phone' src="/Images/globe.png" alt="" />
-                </div>
 
 
             </div>
